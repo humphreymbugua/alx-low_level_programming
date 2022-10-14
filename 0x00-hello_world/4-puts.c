279956213a82e62b936088c58df723e0b3281b4e
humphreymbugua <humphreeydiaz@gmail.com>
2022-10-14 09:26:38 -0700

    a C program that prints txt.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
- * main - entry
- * return: Always 0
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
