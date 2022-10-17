801951a773a342abaecacc152525b31926b8d413
humphreymbugua <humphreeydiaz@gmail.com>
2022-10-16 18:42:47 -0700

    a program that prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,12 +9,12 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num2 < 9; num1++)
+	for (num1 = 0; num2 < 90; num1++)
 	{
-		for (num2 = num1 + 1; num2 < 10; num2++)
+		for (num2 = num1 + 1; num2 < 91; num2++)
 		{
-			putchar((num1 % 10) + '0');
-			putchar((num2 % 10) + '0');
+			putchar((num1 % 90 + '0');
+			putchar((num2 % 90) + '0');
 			
 			if (num1 == 8 && num2 == 9)
 				continue;
