424d0a0258a03dad88ba1277189912ece0c4d091
humphreymbugua <humphreeydiaz@gmail.com>
2022-10-20 09:26:29 -0700

    test that the function positive_or_negative()

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Test that a function gives out the correct output
+ *        whether positive or negative integer
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
