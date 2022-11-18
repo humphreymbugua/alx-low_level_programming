0edd9fba1c140d8844215e5390f4a714068b2a68
humphreymbugua <humphreeydiaz@gmail.com>
2022-11-17 18:02:59 -0800

    simple functions

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,30 +1,63 @@
 #include "3-calc.h"
-#include <string.h>
 
 /**
- * get_op_func - selects the correct operation toperform
- * @s: operation to perform
- * Return: pointer to the correct function
+ * op_add - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: add
  */
-
-int (*get_op_func(char *s))(int, int)
+int op_add(int a, int b)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i;
+	return (a + b);
+}
+/**
+ * op_sub - subctracts two numbers
+ * @a: first number
+ * @b: second number
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
 
-	i = 0;
-	while (ops[i].op != NULL)
+}
+/**
+ * op_mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides two numbers
+ * @a: first number
+ * @b: second number
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
 	{
-		if (strcmp(s, ops[i].op) == 0)
-			break;
-		i++;
+		printf("Error\n");
+		exit(100);
 	}
-	return (ops[i].f);
+	return (a / b);
+}
+/**
+ * op_mod - calculates the module of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: remainder of the division
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
