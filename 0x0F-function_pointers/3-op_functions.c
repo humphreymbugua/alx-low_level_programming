a1458b04f29fb5d323f34320eec11715ab5a0042
humphreymbugua <humphreeydiaz@gmail.com>
2022-11-17 18:10:50 -0800

    simple functions

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,29 +1,72 @@
 #include "3-calc.h"
 
 /**
- * get_op_func - selects the correct function to perform
- * the operation asked by the user
- * @s: char operator
- * Return: pointer to the function that corresponds to the
- * operator
- */
-int (*get_op_func(char *s))(int, int)
+  * op_add - adds two integers
+  * @a: first int
+  * @b: second int
+  *
+  * Return: the sum
+  */
+int op_add(int a, int b)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i = 0;
+	return (a + b);
+}
 
-	while (i < 10)
+/**
+  * op_sub - subtracts b from a
+  * @a: first int
+  * @b: second int
+  *
+  * Return: the difference
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  * op_mul - multiplies two integers
+  * @a: first int
+  * @b: second int
+  *
+  * Return: the product
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  * op_div - divides a by b
+  * @a: first int
+  * @b: second int
+  *
+  * Return: the quotient
+  */
+int op_div(int a, int b)
+{
+	if (b == 0)
 	{
-		if (s[0] == ops->op[i])
-			break;
-		i++;
+		printf("Error\n");
+		exit(100);
 	}
-	return (ops[i / 2].f);
+	else
+		return (a / b);
+}
+/**
+  * op_mod - finds the remainder of a divide by b
+  * @a: first int
+  * @b: second int
+  *
+  * Return: the remainder
+  */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	else
+		return (a % b);
 }
