59dc251bdcb53adcc2ab63d1e2062ff522ff687e
humphreymbugua <humphreeydiaz@gmail.com>
2022-11-17 18:09:06 -0800

    simple functions

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,36 +1,39 @@
 #include "3-calc.h"
 
 /**
- * main - check the code for Holberton School students
- * @argc: argument count
- * @argv: argument vector
- * Return: Always 0
- */
+  * main - performs simple calculations
+  *
+  * @argc: number of arguments passed
+  * @argv: array of arrgc elements
+  *
+  * Return: Success --> return of the operation
+  *           98  --> wrong number of arguments
+  *           99  --> wrong operator, refer to 3-get_op_func.c
+  *          100  --> trying to divide by 0
+  */
 int main(int argc, char *argv[])
 {
-	int a, b;
-	int (*operation)(int, int);
+	int a, b, result;
+	char *op;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2][1])
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	operation = get_op_func(argv[2]);
-	if (operation == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-
-	printf("%d\n", operation(a, b));
+	op = argv[2];
+	result = get_op_func(op)(a, b);
+	if (!result)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	else
+	{
+		printf("%d\n", result);
+	}
 	return (0);
 }
