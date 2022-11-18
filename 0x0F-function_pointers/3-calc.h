61b7a51977e1fca21dec87cea1133f762894681e
humphreymbugua <humphreeydiaz@gmail.com>
2022-11-17 17:57:16 -0800

    simple functions

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,12 +1,13 @@
-#ifndef _CALC_H_
-#define _CALC_H_
+#ifndef _CALC_H
+#define _CALC_H
 
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * struct op - Struct op
  * @op: The operator
  * @f: The function associated
  */
-
 typedef struct op
 {
 	char *op;
@@ -20,4 +21,4 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
-#endif /* _CALC_H_ */
+#endif
