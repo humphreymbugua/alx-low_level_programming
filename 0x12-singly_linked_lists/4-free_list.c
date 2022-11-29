537cc5c3da9174d31bef359e64438f315ef6de87
humphreymbugua <humphreeydiaz@gmail.com>
2022-11-28 18:40:04 -0800

    function that frees a list_t list.

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,20 +1,22 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * free_list - frees a linked list
- * @head: list_t list to be freed
+ * free_list- free dynamically allocated memory of a list node elements
+ * @head: pointer to head of a Linked List
+ *
+ * Return: void
+ * On error, -1 is returned, and errno is set appropriately.
  */
 void free_list(list_t *head)
 {
- list_t *temp;
+	list_t *temp;
 
- while (head)
- {
- temp = head->next;
- free(head->str);
- free(head);
- head = temp;
- }
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+
 }
-
