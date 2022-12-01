79415c288d89e09b7e1e238c691b9003ac34d3ca
humphreymbugua <humphreeydiaz@gmail.com>
2022-12-01 11:12:58 -0800

    a function that prints all the elements of a listint_t list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -9,13 +10,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for project
+ * for alx project
  */
-
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+ int n;
+ struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -34,4 +34,5 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif /* LISTS_H */
+#endif
+
